ba2de981eba3bee56735ce475f99f8be30a67e8c
Lewis Nakao <lewdev@gmail.com>
2021-07-18 23:38:15 -1000

    Fix window resize
    * Attempted to fix slowness isssue after so many games.

--- src/script.js
+++ src/script.js
@@ -9,25 +9,30 @@ FPS = 20,//50fps
 SIZE = 40,
 PIECES_COUNT = 30,
 SPEED = 3,
-KILL_DISTANCE = 5,
+TOUCH_DISTANCE = 5,
 R='🧱',
 P='📃',
 S='✂️',
+gameOn = false,
 emojis = [R, P, S],
-pieces = null,
+pieces = (new Array(90)).fill().map(() => ({o:'', x: 0, y: 0})),
 myInterval = null,
 myTimeout = null;
-window.resize = () => { a.width = innerWidth; a.height = innerHeight; };
-window.resize();
 let init = () => {
-  pieces = [];
-  emojis.map(o => createPieces(o));
-  clearInterval(myInterval);
+  let o;
+  for (i = 0; i < 90; i++) {
+    o = pieces[i];
+    o.o = emojis[i % 3];
+    o.x = r() * innerWidth;
+    o.y = r() * innerHeight
+  }
+  gameOn = true;
   clearTimeout(myTimeout);
-  myInterval = setInterval(update, FPS); //30 fps
+  c.font = SIZE + 'px serif';
 };
-c.font = SIZE + 'px serif';
-let createPieces = o => {for (i=0;i<PIECES_COUNT;i++) pieces.push({o, x: r() * innerWidth, y: r() * innerHeight})}
+const resize = () => {a.width = innerWidth; a.height = innerHeight;}
+w.addEventListener('resize', resize);
+resize();
 
 let clear = () => {
   c.fillStyle = "#888";
@@ -52,30 +57,21 @@ let drawLine = (p1, p2) => {
   c.stroke();
 };
 let pieceMap = {};
-let isDead = o => !pieceMap[o] || pieceMap[o].length === 0;
-let didWin = p => {
-  if (emojis.filter(o => o !== p && isDead(o)).length === 2) {
-    str = p + ' WINS';
-    x = innerWidth / 2 - c.measureText(str).width / 2;//centered
-    y = innerHeight - 100;//bottom
-    c.fillText(str, x, y);
-    return true;
-  }
-  return false;
-};
 let drawScore = (o, y) => {
   c.fillStyle = "black";
   c.fillText(o + ' ' + (pieceMap[o] ? pieceMap[o].length : 0), 30, y);
 };
 let targets, closest, xVelocity, yVelocity, pangle;
 let update = () => {
+  if (!gameOn) return;
   clear();
   pieces.map(p => {
     //find closest opposing piece
-    targets = pieces.filter(p2 => (p.o === R && p2.o === S)
+    targets = pieces.filter(p2 => p2.o && ((
+      p.o === R && p2.o === S)
       || (p.o === P && p2.o === R)
       || (p.o === S && p2.o === P)
-    );
+    ));
     if (targets.length > 0) {
       targets.sort((a, b) => dist(p, a) - dist(p, b));
       closest = targets[0];
@@ -89,34 +85,52 @@ let update = () => {
     }
   });
   //collision detection
+  pieces.map(p => c.fillText(p.o, p.x - SIZE / 2, p.y + SIZE / 2));
   pieces.map(p => {
-    if (p.x < 0) p.x = 0;
-    if (p.x > innerWidth) p.x = innerWidth
-    if (p.y < 0) p.y = 0;
-    if (p.y > innerHeight) p.y = innerHeight;
-    targets = pieces.filter(t => (p.o === R && t.o === S)
+    // if (p.x < 0) p.x = 0;
+    // if (p.x > innerWidth) p.x = innerWidth
+    // if (p.y < 0) p.y = 0;
+    // if (p.y > innerHeight) p.y = innerHeight;
+    targets = pieces.filter(t => p.o && ((p.o === R && t.o === S)
       || (p.o === P && t.o === R)
-      || (p.o === S && t.o === P)
+      || (p.o === S && t.o === P))
     );
     if (targets.length > 0) {
       targets.sort((a, b) => dist(p, a) - dist(p, b));
       closest = targets[0];
-      if (dist(p, closest) < KILL_DISTANCE) closest.remove = 1;
+      // if (dist(p, closest) < TOUCH_DISTANCE) closest.remove = 1;
+      if (dist(p, closest) < TOUCH_DISTANCE) {
+        closest.o = '';
+        c.fillText('💥', p.x - SIZE / 2, p.y + SIZE / 2);
+      }
     }
   });
-  pieces.map(p => c.fillText(p.o, p.x - SIZE / 2, p.y + SIZE / 2));
-  pieces = pieces.filter(p => {
-    if (p.remove) c.fillText('💥', p.x - SIZE / 2, p.y + SIZE / 2);
-    return !p.remove;
+  emojis.map((o, i) => {
+    pieceMap[o] = pieces.filter(p => p.o === o);
+    drawScore(o, 70 + (i * 70));
   });
-  //end condition
-  emojis.map(o => pieceMap[o] = pieces.filter(p => p.o === o));
-  emojis.map((o, i) => drawScore(o, 70 + (i * 70)));
-  if (emojis.filter(o => didWin(o)).length === 1) {
-    clearInterval(myInterval);
+
+  //check end condition
+  if (isEndGame()) {
+    gameOn = false;
     myTimeout = setTimeout(init, 6000);
   }
 };
+setInterval(update, FPS);
+let isEndGame = () => emojis.filter(o => didWin(o)).length === 1;
+let isDead = o => !pieceMap[o] || pieceMap[o].length === 0;
+let didWin = p => {
+  if (emojis.filter(o => o !== p && isDead(o)).length === 2) {
+    c.font = (innerWidth / 6) + 'px serif';
+    str = p + ' WINS';
+    x = innerWidth / 2 - c.measureText(str).width / 2;//centered
+    y = innerHeight / 2;//centered
+    c.fillText(str, x, y);
+    return true;
+  }
+  return false;
+};
+//restart on click, touch, or keypress
 d.onclick=d.ontouchend=d.onkeyup=init;
 
-onload = init;
+onload = () => init();
